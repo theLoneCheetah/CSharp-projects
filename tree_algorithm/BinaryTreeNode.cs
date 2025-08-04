c4d2c95a1ce4714581e9f536488c9b255a7f69fb
theLoneCheetah <105351733+theLoneCheetah@users.noreply.github.com>
2025-08-04 11:49:13 +0300

    Add files via upload

---
+++ tree_algorithm/BinaryTreeNode.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+class BinaryTreeNode
+{
+    private BinaryTreeNode left;
+    private BinaryTreeNode right;
+    private string value;
+
+    public BinaryTreeNode(ref Queue<string> values)
+    {
+        if (values.Count == 0) return;
+
+        this.value = values.Dequeue();
+
+        if (this.value == ".")
+        {
+            this.left = null;
+            this.right = null;
+        }
+        else
+        {
+            this.Add(ref values, false);
+            this.Add(ref values, true);
+        }
+    }
+
+    private void Add(ref Queue<string> values, bool is_right)
+    {
+        if (!is_right)
+            this.left = new BinaryTreeNode(ref values);
+        else
+            this.right = new BinaryTreeNode(ref values);
+    }
+
+    public void PrintTree()
+    {
+        Console.WriteLine("Значение: {0}, левый: {1}, правый: {2}", this.value, this.left.value, this.right.value);
+
+        if (this.left.value != ".")
+            this.left.PrintTree();
+        if (this.right.value != ".")
+            this.right.PrintTree();
+    }
+
+    public void StraightTraversal(ref Queue<string> traversal)
+    {
+        traversal.Enqueue(this.value);
+        if (this.left.value != ".")
+            this.left.StraightTraversal(ref traversal);
+        if (this.right.value != ".")
+            this.right.StraightTraversal(ref traversal);
+    }
+
+    public void CenteredTraversal(ref Queue<string> traversal)
+    {
+        if (this.left.value != ".")
+            this.left.CenteredTraversal(ref traversal);
+        traversal.Enqueue(this.value);
+        if (this.right.value != ".")
+            this.right.CenteredTraversal(ref traversal);
+    }
+
+    public void ReversedTraversal(ref Queue<string> traversal)
+    {
+        if (this.left.value != ".")
+            this.left.ReversedTraversal(ref traversal);
+        if (this.right.value != ".")
+            this.right.ReversedTraversal(ref traversal);
+        traversal.Enqueue(this.value);
+    }
+}
---
+++ tree_algorithm/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    private static void DifferentTraversals()
+    {
+        // a b d . . e . . c . f . .
+        Console.WriteLine("Enter nodes' values starting from root and separated by space:");
+        string[] input = Console.ReadLine().Split(' ');
+        Console.WriteLine();
+
+        Queue<string> values = new Queue<string>(input);
+        BinaryTreeNode root = new BinaryTreeNode(ref values);
+
+        Console.WriteLine("Detailed view of tree:");
+        root.PrintTree();
+        Console.WriteLine();
+
+        Queue<string> traversal = new Queue<string>();
+
+        Console.Write("Straight tree traversal:");
+        root.StraightTraversal(ref traversal);
+        while (traversal.Count > 0)
+            Console.Write(" {0}", traversal.Dequeue());
+        Console.WriteLine();
+
+        Console.Write("Centered tree traversal:");
+        root.CenteredTraversal(ref traversal);
+        while (traversal.Count > 0)
+            Console.Write(" {0}", traversal.Dequeue());
+        Console.WriteLine();
+
+        Console.Write("Reversed tree traversal:");
+        root.ReversedTraversal(ref traversal);
+        while (traversal.Count > 0)
+            Console.Write(" {0}", traversal.Dequeue());
+        Console.WriteLine();
+    }
+
+    private static void MathExpression()
+    {
+        // / * + 2 . . 3 . . - 7 . . 4 . . 3 . .
+        Console.WriteLine("Enter nodes' values starting from root and separated by space:");
+        string[] input = Console.ReadLine().Split(' ');
+        Console.WriteLine();
+
+        Queue<string> values = new Queue<string>(input);
+        BinaryTreeNode root = new BinaryTreeNode(ref values);
+
+        Console.WriteLine("Detailed view of tree:");
+        root.PrintTree();
+        Console.WriteLine();
+
+        Queue<string> traversal = new Queue<string>();
+        root.ReversedTraversal(ref traversal);
+
+        Stack<int> calc = new Stack<int>();
+        int temp, a, b;
+        string sym;
+
+        while (traversal.Count > 0)
+        {
+            sym = traversal.Dequeue();
+            Console.Write("{0} ", sym);
+
+            if (int.TryParse(sym, out temp))
+                calc.Push(temp);
+
+            else
+            {
+                b = calc.Pop();
+                a = calc.Pop();
+
+                switch (sym)
+                {
+                    case "+":
+                        temp = a + b;
+                        break;
+                    case "-":
+                        temp = a - b;
+                        break;
+                    case "*":
+                        temp = a * b;
+                        break;
+                    case "/":
+                        temp = a / b;
+                        break;
+                }
+
+                calc.Push(temp);
+            }
+        }
+
+        Console.WriteLine("= {0}", calc.Pop());
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter 0 to test different tree traversals, 1 to calculate math expression by tree: ");
+        bool mode = Console.ReadLine() == "1";
+        Console.WriteLine();
+
+        if (!mode)
+            DifferentTraversals();
+        else
+            MathExpression();
+    }
+}
